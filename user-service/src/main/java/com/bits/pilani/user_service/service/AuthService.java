828612c3c7b726f0282c84a617675080f7f29440
abhicv <abhijithcheruvery@gmail.com>
2024-11-11 19:45:28 +0530

    Added /auth endpoint for getting JWT bearer token

---
+++ user-service/src/main/java/com/bits/pilani/user_service/service/AuthService.java
@@ -0,0 +1,61 @@
+package com.bits.pilani.user_service.service;
+
+import java.util.Date;
+import java.util.Objects;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.bits.pilani.exception.CustomException;
+import com.bits.pilani.user_service.dao.UserDao;
+import com.bits.pilani.user_service.to.UsernamePasswordTO;
+
+import io.jsonwebtoken.Jwts;
+
+@Service
+public class AuthService {
+		
+	@Autowired
+	UserDao userDao;
+	
+	@Autowired
+	SecretKey secretKey;
+	
+	public String authenticateAndGetToken(UsernamePasswordTO usernamePasswordTO) throws CustomException {        		
+		
+		var userEntity = userDao.findByUsername(usernamePasswordTO.getUsername());
+		
+		if(userEntity == null) {
+			String errorMsg = String.format("Username = '%s' is invalid", usernamePasswordTO.getUsername());
+			throw new CustomException(HttpStatus.UNAUTHORIZED, errorMsg);
+		}
+		
+		if(!userEntity.getPassword().equals(usernamePasswordTO.getPassword())) {
+			throw new CustomException(HttpStatus.UNAUTHORIZED, "Password is invalid");			
+		}
+		
+		String token = Jwts.builder()
+                .subject(usernamePasswordTO.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .claim("userId", userEntity.getId())
+                .claim("roleId", userEntity.getRoleId())
+                .signWith(secretKey)
+                .compact();
+
+        return token;
+	}
+	
+	public void validateUsernamePasswordTO(UsernamePasswordTO usernamePasswordTO) throws CustomException {		
+
+		if(Objects.isNull(usernamePasswordTO.getUsername())) {
+			throw new CustomException(HttpStatus.BAD_REQUEST, "Username is missing. Please provide username.");
+		}
+		
+		if(Objects.isNull(usernamePasswordTO.getPassword())) {
+			throw new CustomException(HttpStatus.BAD_REQUEST, "Password is missing. Please provide password.");
+		}
+	}
+}
