881f5ecb25aa6041b601988a80d80f6b1d0c8eac
Akshara Anilkumar <2023mt93286@wilp.bits-pilani.ac.in>
2024-11-15 15:02:41 +0530

    delivery_details script

---
+++ db_scripts/delivery_details.sql
@@ -0,0 +1,16 @@
+-- create delivery details table
+CREATE TABLE public.delivery_details (
+    order_id integer,
+    delivery_person_id integer,
+    delivered BOOLEAN DEFAULT FALSE,
+    delivery_message VARCHAR(255) DEFAULT 'Not picked up',
+    delivery_accepted BOOLEAN DEFAULT FALSE,
+    CONSTRAINT order_id_fk FOREIGN KEY (order_id) REFERENCES public.Order (order_id),
+    CONSTRAINT delivery_person_id_fk FOREIGN KEY (delivery_person_id) REFERENCES public.user (id) 
+);
+
+-- insert sample details into delivery details table
+INSERT INTO public.delivery_details (order_id,delivery_person_id,delivered,delivery_accepted) VALUES 
+    ('1','3',false,'false');
+INSERT INTO public.delivery_details (order_id,delivery_person_id,delivered,delivery_accepted) VALUES 
+    ('2','3',false,'false');
