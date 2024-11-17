7158bc81b4fbfa95486ea75184982898e1a3a133
csaradhi-tejas-wilp <2023mt93207@wilp.bits-pilani.ac.in>
2024-11-17 12:02:48 +0530

    Scan base packages

--- order-service/src/main/java/com/bits/pilani/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/bits/pilani/orderservice/OrderServiceApplication.java
@@ -3,7 +3,7 @@ package com.bits.pilani.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bits.pilani")
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
